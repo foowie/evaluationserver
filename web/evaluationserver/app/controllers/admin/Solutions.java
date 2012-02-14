14f91cf390ba4c873739a705a592a40606ba0b22
Foowie <foowie@gmail.com>
2012-02-14 19:26:52 +0100

    Fix solution file management, add posibility to remove evaluation from solution

--- web/evaluationserver/app/controllers/admin/Solutions.java
+++ web/evaluationserver/app/controllers/admin/Solutions.java
@@ -2,8 +2,13 @@ package controllers.admin;
 
 import controllers.CRUD;
 import controllers.Check;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import models.Role;
+import models.SolutionFile;
 import play.mvc.Before;
+import play.mvc.Router;
 import play.mvc.With;
 
 @Check(Role.Check.ADMIN)
@@ -20,6 +25,41 @@ public class Solutions extends CRUD {
 			params.put("orderBy", "id");
 			params.put("order", "DESC");
 		}
+	}
+	
+	@Before(only="save")
+	public static void beforeSave(Long id, java.io.File file) throws IOException {
+        models.Solution solution = models.Solution.findById(id);
+		notFoundIfNull(solution);
+
+		if(file != null) {
+			SolutionFile solutionFile = new SolutionFile(file);
+			solutionFile.save();
+			params.put("object.file.id", solutionFile.getId().toString());
+		}
+	}	
+	
+    public static void blank() throws Exception {
+		redirect(Router.getFullUrl("admin.Solutions.list"));
+	}
+    public static void create() throws Exception {
+		redirect(Router.getFullUrl("admin.Solutions.list"));
+	}
+	
+	public static void unevaluate(Long id) {
+        models.Solution solution = models.Solution.findById(id);
+		notFoundIfNull(solution);
+		solution.evaluated = null;
+		solution.evaluationLockUntil = null;
+		solution.log = null;
+		solution.memory = null;
+		solution.systemReply = null;
+		solution.timeLength = null;
+		solution.save();
+		
+		Map<String, Object> parameters = new HashMap<String, Object>();
+		parameters.put("id", id);
+		redirect(Router.getFullUrl("admin.Solutions.show", parameters));
 	}
 	
 }
